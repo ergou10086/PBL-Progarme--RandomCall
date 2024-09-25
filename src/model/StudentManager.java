ddbdd95d015fb2ca62af8a117a17fe267eabfc89
aliyun2191685471 <zjm88822201@126.com>
2024-09-25 14:54:41 +0800

    更新 StudentManager.java

--- src/model/StudentManager.java
+++ src/model/StudentManager.java
@@ -1,12 +1,15 @@
 package model;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
 // 学生列表管理类，维护学生数据
 public class StudentManager {
-    // 用来存储Student对象的集合，增删改
-    private List<Student> students;
+    // 用来存储Student对象的集合，增删
+    private List<Student> students = new ArrayList<>();
+    // IO方法
+    private final String FILE_NAME = "students.dat";
 
     // 构造方法,声明students这个集合
     public StudentManager() {
@@ -16,15 +19,34 @@ public class StudentManager {
     // 增
     public void addStudent(Student student) {
         students.add(student);
+        saveStudents();
     }
 
     // 删
     public void removeStudent(Student student) {
         students.remove(student);
+        saveStudents();
     }
 
     // getter
     public List<Student> getStudents() {
+        loadStudents();
         return students;
+    }
+
+    private void saveStudents() {
+        try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(FILE_NAME))) {
+            oos.writeObject(students);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void loadStudents() {
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(FILE_NAME))) {
+            students = (List<Student>) ois.readObject();
+        } catch (IOException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
